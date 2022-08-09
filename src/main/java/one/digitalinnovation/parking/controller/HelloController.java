ece6392e228f9ca7e50fcbc52e35ddc2f7af2cb9
Tomjesus <aeltontj@gmail.com>
2022-08-08 23:15:41 -0300

    Swagger não apresenta bom fucionamento, vesão do springfox não é mais compativel.Foi substituido pelo springdoc.

--- src/main/java/one/digitalinnovation/parking/controller/HelloController.java
+++ src/main/java/one/digitalinnovation/parking/controller/HelloController.java
@@ -1,9 +1,17 @@
 package one.digitalinnovation.parking.controller;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
+
+import java.lang.annotation.Retention;
 
 @RestController
+@RequestMapping("/")
+@ApiIgnore
 public class HelloController {
     @GetMapping
     public String hello(){
--- src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
+++ src/main/java/one/digitalinnovation/parking/controller/ParkingController.java
@@ -1,18 +1,25 @@
 package one.digitalinnovation.parking.controller;
 
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import one.digitalinnovation.parking.controller.dto.ParkingCreateDTO;
 import one.digitalinnovation.parking.controller.dto.ParkingDTO;
 import one.digitalinnovation.parking.controller.mapper.ParkingMapper;
 import one.digitalinnovation.parking.model.Parking;
 import one.digitalinnovation.parking.service.ParkingService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/parking")
-public class ParkingController {
+@Api(tags = "Paeking Controller" )
+public class  ParkingController {
 
 
     private final ParkingService parkingService;
@@ -24,12 +31,30 @@ public class ParkingController {
     }
 
     @GetMapping
-    private List<ParkingDTO> findAll(){
+    @ApiOperation("Find all parkings")
+   public ResponseEntity<List<ParkingDTO>> findAll() {
 
         List<Parking> parkingList = parkingService.findAll();
         List<ParkingDTO> result = parkingMapper.toParkingDTOList(parkingList);
-        return result;
+        return ResponseEntity.ok(result);
 
+
+    }
+    @GetMapping("/{id}")
+   public ResponseEntity<ParkingDTO> findById(@PathVariable String id){
+
+        Parking parking = parkingService.findById(id);
+        ParkingDTO result = parkingMapper.toParkingDTO(parking);
+        return ResponseEntity.ok(result);
+
+
+    }
+    @PostMapping
+    public ResponseEntity<ParkingDTO> create(@RequestBody ParkingCreateDTO dto){
+        Parking parkingCreate = parkingMapper.toParkingCreate(dto);
+        Parking parking = parkingService.create(parkingCreate);
+        ParkingDTO result = parkingMapper.toParkingDTO(parking);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
 
 
     }
